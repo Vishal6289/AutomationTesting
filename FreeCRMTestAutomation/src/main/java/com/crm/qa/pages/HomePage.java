ab01b30e53d418177eb474bb5912545c5ea6e326
Vishal <vishal.d.hell26@gmail.com>
2023-04-06 21:05:05 +0530

    New update

--- FreeCRMTestAutomation/src/main/java/com/crm/qa/pages/HomePage.java
+++ FreeCRMTestAutomation/src/main/java/com/crm/qa/pages/HomePage.java
@@ -1,4 +1,13 @@
 package com.crm.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePage {
+    LoginPage loginpage;
+
+    @FindBy(xpath = "//*[@id=\'top-header-menu\']/div[2]/span[1]")
+    WebElement UserNameLabel;
+
+
 }
